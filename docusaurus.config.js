0621196cf6d4c6ae25812142097af06005c0bdc7
yashdev9274 <yashdev.yvd@gmail.com>
2023-10-04 13:36:49 +0530

    site name update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,7 +66,7 @@ const config = {
       // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: "My Site",
+        title: "BuildersChain",
         logo: {
           alt: "My Site Logo",
           src: "img/BuildersChain_Profile.jpeg",
